6d2e098f533fcf2c7191c3d2ab66a438b741645b
Harshit Singh <singhjdk299288@gmail.com>
2023-05-13 21:10:17 +0530

    bug fix

--- src/components/profilePage/UserProfilePage.js
+++ src/components/profilePage/UserProfilePage.js
@@ -1,30 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './UserProfilePage.css'
 import avatar from '../middleSection/avatar.jpg'
 const UserProfilePage = () => {
-  const profileData = {
-    id: 1,
-    first_name: 'Jolie',
-    last_name: 'Ferne',
-    email: 'jferne0@website.com',
-    gender: 'Female',
-    country: 'Sweden',
+  const [profileData, setProfileData] = useState(null);
+  useEffect(() => {
+    fetchProfileData();
+  }, []);
+  const fetchProfileData = async () => {
+    try {
+      const response = await fetch('https://tweets.free.beeceptor.com/profile'); // Replace 'API_URL' with the actual URL of your API
+      const data = await response.json();
+      setProfileData(data);
+    } catch (error) {
+      console.log('Error fetching profile data:', error);
+    }
   };
-
   return (
     <div className="profile-page">
-      <div className="profile-header">
-        <div className="profile-picture">
-          <img src={avatar} alt="Profile" />
+      {profileData ? (
+        <div className="profile-header">
+          <div className="profile-picture">
+            <img src={avatar} alt="Profile" />
+          </div>
+          <div className="profile-info">
+            <h1>{`${profileData.first_name} ${profileData.last_name}`}</h1>
+            <p>Email: {profileData.email}</p>
+            <p>Gender: {profileData.gender}</p>
+            <p>Country: {profileData.country}</p>
+          </div>
         </div>
-        <div className="profile-info">
-          <h1>{`${profileData.first_name} ${profileData.last_name}`}</h1>
-          <p>Email: {profileData.email}</p>
-          <p>Gender: {profileData.gender}</p>
-          <p>Country: {profileData.country}</p>
-        </div>
-      </div>
-      
+      ) : (
+        <p>Loading profile data...</p>
+      )}
     </div>
   );
 };
